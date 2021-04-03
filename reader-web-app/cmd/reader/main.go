46e44013c2d0c21827d05f91a1ea6520231de409
Nanda Kishore <battaru.nandakishore@bigbasket.com>
2021-04-03 23:40:00 +0530

    First version of reader

--- reader-web-app/cmd/reader/main.go
+++ reader-web-app/cmd/reader/main.go
@@ -7,13 +7,38 @@ import (
 	"os"
 	"time"
 
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/none-da/try-online-schema-change/reader-web-app/pkg/reader"
+	log "github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
 )
 
 // StartTime gives the start time of server
 var StartTime = time.Now()
 
 const defaultAppPort string = "8080"
+
+func validateEnvVars() map[string]string {
+	var requiredDbEnvVars = [5]string{
+		"DB_NAME",
+		"DB_HOST",
+		"DB_USER",
+		"DB_PASSWORD",
+		"DB_PORT"}
+
+	dbConfig := make(map[string]string)
+	for _, envVar := range requiredDbEnvVars {
+		envVarValue, isSet := os.LookupEnv(envVar)
+		if !isSet {
+			log.Fatal(fmt.Sprintf("%s is a required env variable. Please set it and with a valid value ofcourse.", envVar))
+		}
+		dbConfig[envVar] = envVarValue
+	}
+
+	return dbConfig
+}
 
 func uptime() string {
 	elapsedTime := time.Since(StartTime)
@@ -25,14 +50,67 @@ func homePage(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, fmt.Sprintf("[HOST: %s] (uptime: %s)]", host, uptime()))
 }
 
-func readData(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, reader.ReadData())
+func (env *Env) readData(w http.ResponseWriter, r *http.Request) {
+	baseItem := reader.ReadData(env.db)
+	fmt.Fprintf(w, fmt.Sprintf("created_on: %s updated_on: %s product_id: %s", baseItem.CreatedOn, baseItem.UpdatedOn, baseItem.ProductID))
+}
+
+// Create a custom Env struct which holds a connection pool.
+type Env struct {
+	db *sql.DB
 }
 
 func main() {
+	var requiredLogLevel string
+	flag.StringVarP(&requiredLogLevel, "log-level", "l", "info", "Required log level. debug/info/warn/error.")
+	var printHelp bool
+	flag.BoolVarP(&printHelp, "help", "h", false, "Prints this help content.")
+	maxOpenConnectionsToDbPtr := flag.Int("max-open-conns-db", 200, "Max Open Connections to the Db.")
+	maxIdleConnectionsToDbPtr := flag.Int("max-idle-conns-db", 10, "Max Idle Connections to the Db.")
+	var sleepTimeBetweenInsertsPtr int64
+	flag.Int64VarP(&sleepTimeBetweenInsertsPtr, "sleep-between-inserts", "s", 0, "sleep time in seconds between inserts, to do db commits")
+	var rowsToInsert int64
+	flag.Int64VarP(&rowsToInsert, "rows-to-insert", "r", 1, "Rows to insert.")
+	flag.Parse()
+	if printHelp {
+		flag.Usage()
+		return
+	}
+	switch requiredLogLevel {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.SetLevel(log.InfoLevel)
+	}
+	log.SetFormatter(&log.JSONFormatter{
+		TimestampFormat:   time.RFC3339,
+		DisableHTMLEscape: true,
+	})
+	log.SetOutput(os.Stdout)
+	log.Info("I am a reader")
+
+	dbConfig := validateEnvVars()
+
+	dsnString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dbConfig["DB_USER"], dbConfig["DB_PASSWORD"], dbConfig["DB_HOST"], dbConfig["DB_PORT"], dbConfig["DB_NAME"])
+	log.Debug("dsnString: ", dsnString)
+	db, err := sql.Open("mysql", dsnString)
+	reader.CheckErr(err)
+	db.SetMaxOpenConns(*maxOpenConnectionsToDbPtr)
+	db.SetMaxIdleConns(*maxIdleConnectionsToDbPtr)
+
+	err = db.Ping()
+	reader.CheckErr(err)
+
+	env := &Env{db: db}
 
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/read", readData)
+	http.HandleFunc("/read", env.readData)
 
 	http.ListenAndServe(fmt.Sprintf(":%s", defaultAppPort), nil)
 }
--- reader-web-app/pkg/reader/main.go
+++ reader-web-app/pkg/reader/main.go
@@ -1,5 +1,36 @@
 package reader
 
-func ReadData() string {
-	return "I would read data"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func CheckErr(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+type BaseItem struct {
+	CreatedOn string
+	UpdatedOn string
+	ProductID string
+}
+
+func ReadData(db *sql.DB) *BaseItem {
+	selectQuery := "select created_on, updated_on, product_id from try_osc.baseitem where id >= 1 limit 1;"
+	log.Debug(selectQuery)
+	results, err := db.Query(selectQuery)
+	CheckErr(err)
+	defer results.Close()
+
+	var bi BaseItem
+	results.Next()
+	err = results.Scan(&bi.CreatedOn, &bi.UpdatedOn, &bi.ProductID)
+	CheckErr(err)
+
+	return &bi
 }
